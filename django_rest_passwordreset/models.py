17b8ad66bc21f362fe623e14b7b2df47b40a4e52
Zeno Trevisan <zeno.trevisan@gmail.com>
2019-12-10 19:22:37 -1000

    Corrected import for Django 3

--- django_rest_passwordreset/models.py
+++ django_rest_passwordreset/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils.six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
 
